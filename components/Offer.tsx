c654542727a8b65a47abc6f9a23cff67199bbdbb
vojta <vojta.cina@seznam.cz>
2021-02-26 12:57:39 +0100

    feat splátky

--- components/Offer.tsx
+++ components/Offer.tsx
@@ -4,21 +4,22 @@ import ItemTickbox from '../components/Form/ItemTickbox'
 export default function Offer() {
 
     let [items, setItems] = useState([
-        { name: "Grafika na míru vašim představám", active: false, price: 5034 },
-        { name: "Tvorba loga a brand identity", active: false, price: 7000 },
-        { name: "Vytvoření textů na web (copywriting)", active: false, price: 3200 },
-        { name: "Mapa prodejny nebo podniku", active: false, price: 732 },
-        { name: "Fotogalerie", active: false, price: 1402 },
-        { name: "Recenze", active: false, price: 723 },
-        { name: "Modul novinky", active: false, price: 3607 },
-        { name: "Aktuální otevírací doba", active: false, price: 510 },
+        { name: "Grafika na míru vašim představám", active: false, price: 5000 },
+        { name: "Automatické emaily zákazníkům", active: false, price: 6000 },
+        { name: "Vytvoření textů na web (copywriting)", active: false, price: 3000 },
+        { name: "Mapka prodejny nebo poboček", active: false, price: 400 },
+        { name: "Fotogalerie", active: false, price: 1500 },
+        { name: "Recenze", active: false, price: 500 },
+        { name: "Modul novinky", active: false, price: 2000 },
+        { name: "Aktuální otevírací doba", active: false, price: 600 },
         { name: "Kalkulace ceny nebo poptávkový formulář", active: false, price: 2400 },
-        { name: "Vícejazyčný web", active: false, price: 6732 },
-        { name: "Rozsáhlý web (20+ stránek)", active: false, price: 6367 },
-        { name: "Uživatelská sekce", active: false, price: 8673 },
-        { name: "Napojení na sociální sítě", active: false, price: 210 },
-        { name: "Měření návštěvnosti", active: false, price: 49 },
-        { name: "Možnost měnit texty na webu", active: false, price: 4500 },
+        { name: "Vícejazyčný web", active: false, price: 7000 },
+        { name: "Rozsáhlý web (20+ stránek)", active: false, price: 4000 },
+        { name: "Uživatelská sekce", active: false, price: 9000 },
+        { name: "Vytvoření firemního e-mailu", active: false, price: 600 },
+        { name: "Měření návštěvnosti a statistiky", active: false, price: 100 },
+        { name: "Možnost měnit texty na webu (vlastní administrace)", active: false, price: 4500 },
+        { name: "Web na splátky (12 měsíců)", active: false, price: 1000 },
     ])
 
 
@@ -35,7 +36,21 @@ export default function Offer() {
             sum = sum + theLine.price
         })
 
+        if (newArr.filter(row => row.name == "Web na splátky (12 měsíců)")[0].active == true) {
+            sum = Math.floor(sum/12)
+        }
+
         return (sum)
+    }
+
+    function naSplatky() {
+        let newArr = [...items]; // copying the old datas array
+        if (newArr.filter(row => row.name == "Web na splátky (12 měsíců)")[0].active == true) {
+            return (" měsíčně")
+        }
+        else { 
+            return ("")
+        }
     }
 
 
@@ -52,10 +67,10 @@ export default function Offer() {
                     )}
                 </div>
                 <div className="w-full text-center flex items-center justify-center my-12px">
-                    <span>Vaše cena: </span><span className="ml-6px text-24 font-medium">{SumPrice()} Kč</span>
+                    <span>Vaše cena: </span><span className="ml-6px text-24 font-medium">{SumPrice()} Kč </span>&nbsp; {naSplatky()}
                 </div>
                 <div className="w-full h-1px bg-black"></div>
-                <div className="w-full p-12px text-12">Součástí webu je <strong>základní grafický návrh, responzivita, doména a hosting, SEO optimalizace a správa</strong></div>
+                <div className="w-full p-12px text-12">Součástí webu je vždy základní grafický návrh, responzivita, doména a hosting, SEO optimalizace a správa.</div>
 
 
 
