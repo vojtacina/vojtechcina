2259c71978cef30b4050e50bb824a30c1a242cf0
vojta <vojta.cina@seznam.cz>
2021-03-03 16:59:29 +0100

    offer vylepšení

--- components/Offer.tsx
+++ components/Offer.tsx
@@ -40,55 +40,57 @@ export default function Offer() {
             sum = sum + theLine.price
         })
 
-       /*  if (newArr.filter(row => row.name == "Web na splátky (12 měsíců)")[0].active == true) {
-            sum = Math.floor(sum/12)
-        } */
+        /*  if (newArr.filter(row => row.name == "Web na splátky (12 měsíců)")[0].active == true) {
+             sum = Math.floor(sum/12)
+         } */
 
         return (sum)
     }
 
     function naSplatky() {
-      /*   let newArr = [...items]; // copying the old datas array
-        if (newArr.filter(row => row.name == "Web na splátky (12 měsíců)")[0].active == true) {
-            return (" měsíčně")
-        }
-        else { 
-            return ("")
-        } */
+        /*   let newArr = [...items]; // copying the old datas array
+          if (newArr.filter(row => row.name == "Web na splátky (12 měsíců)")[0].active == true) {
+              return (" měsíčně")
+          }
+          else { 
+              return ("")
+          } */
 
         return ("")
     }
 
 
     return (
-        <div className="w-full flex justify-center px-20px md:px-36px transform  -translate-y-60px">
-            <div className="w-full max-w-6xl bg-dark  rounded-lg ">
-                <div className="w-full text-center mt-20px font-semibold ">Váš nový web na míru může obsahovat:</div>
-                <div className={"grid grid-cols-1 md:grid-cols-2 mt-20px max-h-full overflow-hidden relative transition-all duration-100"+ (!opened ? " h-400px md:h-200px mb-0" : " mb-20px ")}>
-                    {items.map((line, i) =>
-                        <div key={i + line.name} className="px-30px py-6px">
-                            <ItemTickbox data={line} setter={setter} />
-                        </div>
+        <div>
+            <div className="w-full flex justify-center px-20px md:px-36px transform  -translate-y-60px">
+                <div className="w-full max-w-6xl bg-dark  rounded-lg ">
+                    <div className="w-full text-center mt-20px font-semibold ">Váš nový web na míru může obsahovat:</div>
+                    <div className={"grid grid-cols-1 md:grid-cols-2 mt-20px max-h-full overflow-hidden relative transition-all duration-100" + (!opened ? " h-400px md:h-200px mb-0" : " mb-20px ")}>
+                        {items.map((line, i) =>
+                            <div key={i + line.name} className="px-30px py-6px">
+                                <ItemTickbox data={line} setter={setter} />
+                            </div>
 
-                    )}
-                    <div className={(opened && "hidden ")+" absolute left-0 right-0 bottom-0 h-80px cursor-pointer gradient-more transition duration-150 flex justify-center items-end font-medium transform hover:scale-105 pb-20px"} onClick={() => setOpened(true)}>▼&nbsp;&nbsp;Zobrazit vše&nbsp;&nbsp;▼</div>
+                        )}
+                        <div className={(opened && "hidden ") + " absolute left-0 right-0 bottom-0 h-80px cursor-pointer gradient-more transition duration-150 flex justify-center items-end font-medium transform hover:scale-105 pb-20px"} onClick={() => setOpened(true)}>▼&nbsp;&nbsp;Zobrazit vše&nbsp;&nbsp;▼</div>
+                    </div>
+                    <div className="w-full h-1px bg-black"></div>
+                    <div className="w-full text-center flex items-center justify-center my-12px">
+                        <span>Cena za web na míru: </span><span className="ml-6px text-24 font-medium">{SumPrice()} Kč </span>&nbsp; {naSplatky()}
+                    </div>
+
+
+
+
+
+
+
                 </div>
-                <div className="w-full h-1px bg-black"></div>
-                <div className="w-full text-center flex items-center justify-center my-12px">
-                    <span>Cena za web na míru: </span><span className="ml-6px text-24 font-medium">{SumPrice()} Kč </span>&nbsp; {naSplatky()}
-                </div>
-                { opened && 
-                <div>
-                <div className="w-full h-1px bg-black appear"></div>
-                <div className="w-full p-12px text-12 text-gray-400 appear">Součástí webu je vždy základní grafický návrh, responzivita, doména a hosting, SEO optimalizace a správa.</div>
-                </div>
+            </div>
+            <div className="w-full flex justify-center px-20px md:px-36px transform  -translate-y-60px pt-6px">
+                {opened &&
+                    <div className="max-w-6xl p-12px text-12 text-gray-400 appear">Součástí webu je vždy základní grafický návrh, responzivita, doména a hosting, SEO optimalizace a správa.</div>
                 }
-                
-
-
-
-
-
             </div>
         </div>
     )
