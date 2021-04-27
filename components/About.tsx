9150b111e49a049331dec6c9967179100d6f722f
vojta <vojta.cina@seznam.cz>
2021-04-27 13:26:16 +0200

    updated offer

--- components/About.tsx
+++ components/About.tsx
@@ -16,22 +16,23 @@ export default function About() {
                     </div>
                     <div className="md:w-2/3  flex items-center order-1 md:order-2">
                         <div className="py-30px">
-                            <div>Jmenuji se Vojtěch Cina a nabízím vám služby v oblasti tvorby webových stránek na míru včetně grafického designu. Dále nabízím grafické návrhy tiskovin, webové a mediální grafiky.</div>
+                            <div>Jmenuji se Vojtěch Cina a nabízím vám služby v oblasti tvorby webových stránek na míru včetně grafického designu. </div>
+                            <div className="mt-6px">Ukázkové reference zašlu na vyžádání (kvůli GDPR). </div>
                             <div className="py-20px text-18 font-light">Úspěchy v soutěžích:</div>
                             <div className="">
                                 <ul className="list-disc pl-30px">
-                                    <li><span className="font-bold">1. místo</span> v celostátním kole soutěže v programování kancelářských aplikací </li>
-                                    <li><span className="font-bold">1. místo</span> Talent Olomouckého kraje v technickém a dovednostním oboru  </li>
-                                    <li><span className="font-bold">25. místo</span> v celostátním kole soutěže v programování webových stránek   </li>
-                                    <li><span className="font-bold">4. místo</span> v krajském kole soutěže v programování webových stránek   </li>
-                                    <li><span className="font-bold">1. místo</span>  v okresním kole soutěže v programování webu   </li> 
+                                    <li><span className="font-semibold">1. místo</span> v celostátním kole soutěže v programování kancelářských aplikací </li>
+                                    <li><span className="font-semibold">1. místo</span> Talent Olomouckého kraje v technickém a dovednostním oboru  </li>
+                                    <li><span className="font-semibold">25. místo</span> v celostátním kole soutěže v programování webových stránek   </li>
+                                    <li><span className="font-semibold">4. místo</span> v krajském kole soutěže v programování webových stránek   </li>
+                                    <li><span className="font-semibold">1. místo</span>  v okresním kole soutěže v programování webu   </li> 
                                 </ul>
                             </div>
                             <div className="py-20px text-18 font-light">Kontaktní údaje:</div>
                             <div className="">
                                 <ul className="list-disc pl-30px">
                                     <li>Úvoz 445/116 Brno-Veveří</li>
-                                    <li className="text-green-400 font-semibold">jsem@vojtechcina.cz</li>
+                                    <li>jsem@vojtechcina.cz</li>
                                     <li>+420 774 148 327</li>
                                     <li>IČO: 09902287</li>
                                 </ul>
--- components/Graph.tsx
+++ components/Graph.tsx
@@ -55,7 +55,7 @@ interface SelectProps {
 function SelectGoodOrBadWeb({ firstActive, setFirstActive }: SelectProps) {
     return (
         <div className="flex bg-gray-700 font-medium text-black rounded-lg text-14 my-30px select-none cursor-pointer" onClick={() => setFirstActive(!firstActive)}>
-            <div className={(firstActive && "bg-gray-300 shadow-big ") + " py-6px px-12px  rounded-lg transition duration-200 z-20"}>Moderní web</div>
+            <div className={(firstActive && "bg-gray-300 shadow-big ") + " py-6px px-12px  rounded-lg transition duration-200 z-20"}>Web ode mě</div>
             <div className={(!firstActive && "bg-gray-300 shadow-big ") + " py-6px px-12px  rounded-lg transition duration-200  z-20"}>Klasický web</div>
         </div>
     )
@@ -68,8 +68,9 @@ export default function Graph() {
     return (
         <div className="w-full flex justify-center px-20px my-60px">
             <div className="w-full max-w-6xl flex flex-col items-center">
-                <h2 className="w-full font-medium text-24 text-center py-20px">{activeFirstPage ? "Co dělá weby úspěšné" : "Co nedělá weby úspěšné"}</h2>
-                <div className="w-full md:w-1/3 text-16 text-center">{activeFirstPage ? "Úspěšný je web, který Vám přináší prodeje tím i jasný zisk. Umím se o to postarat." : "Někdo dělá weby opačným způsobem a nezaměřuje se na skutečné kvality webu."}</div>
+                <h2 className="w-full font-medium text-24 text-center py-20px">{activeFirstPage ? "Proč web ode mě?" : "Jak dělají weby ostatní"}</h2>
+                <div className="w-full md:w-1/3 text-16 text-center">{activeFirstPage ? "Umím udělat web, který Vám přináší prodeje tím i jasný zisk. Postarám se o to." : "Někdo dělá weby opačným způsobem a zaměřuje se pouze na vzhled a ego. Takový web ale nefunguje."}</div>
+                <div className="w-full mt-6px md:w-1/3 text-16 text-center">{activeFirstPage ? <span>Kvalita webu = více než <span className="text-green-400 font-semibold">85 %</span></span> : <span>Kvalita webu = méně než <span className="text-red-400 font-semibold">45 %</span></span>}</div>
                 <SelectGoodOrBadWeb firstActive={activeFirstPage} setFirstActive={(what: boolean) => setActiveFirstPage(what)} />
                 <div className="w-full grid grid-cols-1 md:grid-cols-3">
                     <div className="block h-300px md:h-full mb-20px md:mb-0 f w-full">
@@ -78,21 +79,30 @@ export default function Graph() {
                         </div>
                     </div>
                     <div className="block  md:pl-30px px-12px md:pr-0 h-full ">
-                        <Label color={activeFirstPage ? "bg-green-400" : "bg-gray-400"} heading="SEO optimalizace" text="Pokud to s webem myslíte vážně, váš web musí být vyhledatelný na internetu. Až vám někdo bude říkat, že nejdůležitější je grafika, reference a kontaktní formulář, šeredně se plete." />
-                        <Label color={activeFirstPage ? "bg-green-500" : "bg-gray-500"} heading="Reklama" text="Reklama představuje snadný a efektivní způsob, jak získat návštěvníky webu. Pokud ale chcete co nejdříve generovat příjem, rozhodně zvažte investici do kvalitní reklamy." />
-                        <Label color={activeFirstPage ? "bg-green-600" : "bg-red-400"} heading="Uživatelský zážitek a grafika" text="Responzivní a rychlý web je základ. Přehledný a čistý vzhled je dokonalost. Hnusné weby nedělám. Uvidíte." />
+                        <Label color={activeFirstPage ? "bg-green-400" : "bg-gray-400"} heading="SEO optimalizace" text="Pokud to s webem myslíte vážně, váš web musí být vyhledatelný na internetu. Využijte celý potenciál neplaceného dosahu." />
+                        <Label color={activeFirstPage ? "bg-green-500" : "bg-gray-500"} heading="Reklama" text="Reklama představuje snadný a efektivní způsob, jak rychle získat návštěvníky webu a získat výhodu před konkurencí. Pokud chcete co nejdříve generovat příjem, rozhodně zvažte investici do kvalitní reklamy." />
+                        <Label color={activeFirstPage ? "bg-green-600" : "bg-red-400"} heading="Uživatelský zážitek a grafika" text="Responzivní a rychlý web je základ. Přehledný a čistý vzhled je dokonalost. Nenechte design hrát proti Vám. Uživatelský zážitek musí být příjemný. Hnusné weby nedělám. Uvidíte." />
                     </div>
                     <div className="block md:pl-30px px-12px md:pr-0 h-full ">
-                        <Label color={activeFirstPage ? "bg-green-700" : "bg-red-500"} heading="Stránka referencí" text="Na reference se zákazníci dívají až jako na poslední věc. Pomohou, ale nejsou zásadní." />
+                        <Label color={activeFirstPage ? "bg-green-700" : "bg-red-500"} heading="Stránka referencí" text="Na reference se zákazníci dívají až jako na poslední věc. Klíčové je ale to, co se stane před tím." />
                         <Label color={activeFirstPage ? "bg-gray-500" : "bg-red-600"} heading="Kontaktní formulář" text="Stačí uvést email a telefon. Lepší řešení je on-line kalkulace ceny, se kterou vám rád pomohu." />
-                        <Label color={activeFirstPage ? "bg-gray-600" : "bg-red-700"} heading="Sekce blog nebo novinky" text="Web, který generuje zisk, nepotřebuje novinky. Nikdo je nečte." />
-                        <Label color={activeFirstPage ? "bg-gray-700" : "bg-red-800"} heading="Efekty, animace a speciální funkce" text="Na doplňkových efektech úspěch webu nezávisí." />
+                        <Label color={activeFirstPage ? "bg-gray-600" : "bg-red-700"} heading="Sekce blog nebo novinky" text="Web, který generuje zisk, nepotřebuje novinky. Nikdo je nečte. Investujte čas raději do produktu." />
+                        <Label color={activeFirstPage ? "bg-gray-700" : "bg-red-800"} heading="Efekty, animace a speciální funkce" text="Web není šperk, který má být dokonalý. Má fungovat a prodávat. A o to se postarám." />
                         <Label color={activeFirstPage ? "bg-gray-800" : "bg-red-900"} heading="O nás" text="Historické údaje o vaší firmě prodeje nezvýší." />
                     </div>
                 </div>
                 <div className="w-full flex items-center flex-col mt-50px">
                     <div className="text-center text-16 font-semibold">Tak co, zkusíme to spolu?</div>
-                    <div className="text-center text-16 font-regular">Nabídku pro vás vytvořím nezávazně a zdarma.</div>
+                    <div className="text-center text-16 font-regular">Pokud nebudete spokojeni, vrátím Vám bez jediného kecu peníze.</div>
+                    <div className="flex flex-col items-start my-12px text-green-300">
+                        <div >√&nbsp;&nbsp;Konzultace a poradenství správné strategie</div>
+                        <div >√&nbsp;&nbsp;Vytvoření grafického návrhu</div>
+                        <div >√&nbsp;&nbsp;Vytvoření kompletního webu</div>
+                        <div >√&nbsp;&nbsp;SEO optimalizace v ceně</div>
+                        <div >√&nbsp;&nbsp;Doména ke každému webu na 1 rok zdarma</div>
+                        <div >√&nbsp;&nbsp;Neomezený počet revizí</div>
+                        <div >√&nbsp;&nbsp;Možnost propagace na Google Ads a Facebooku</div>
+                    </div>
                     <div className="text-center text-16 font-regular">Napište mi na e-mail:</div>
                     <Link href="mailto:jsem@vojtechcina.cz">
                         <div className="text-center text-16 font-regular h-65px bg-green-500 flex items-center justify-center px-50px rounded-lg my-20px font-medium select-none cursor-pointer hover:bg-green-400">jsem@vojtechcina.cz</div>
