0af594566f71f587dad4036dedc409391787477d
vojta <vojta.cina@seznam.cz>
2021-08-12 18:20:02 +0200

    fix size

--- components/Leading.tsx
+++ components/Leading.tsx
@@ -4,7 +4,7 @@ export default function Leading() {
     return (
         <div className="w-full relative" > 
             <div className="main-bg w-full text-center h-300px md:h-400px  relative ">
-                <div className={"z-20 flex flex-col items-center justify-center h-full transition duration-300 transform pt-60px "}>
+                <div className={"z-20 flex flex-col items-center justify-center h-full transition duration-300 translate-y-20px transform "}>
                     <div className="block text-gray-400"></div>
                     <div className="text-24 sm:text-36 lg:text-55 xl:text-64  font-semibold block w-full px-12px font-quicksand"><h1>Vojtěch Cina - webový vývojář</h1></div>
                     <div className="block w-2/3 text-gray-200" >pomohu vám uspět v on-line světě. Klidně ještě <span className="uppercase px-4px py-2px bg-green-500 font-semibold rounded text-white text-14">DNES</span>.</div>
